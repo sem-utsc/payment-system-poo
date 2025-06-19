ab8755c9edc506004bd1da2dd44d8c160a255458
sem <156494699+sem-utsc@users.noreply.github.com>
2025-06-19 23:56:48 +0000

    Add my view and my route site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// GET My View page
+router.get("/my-page", function (req, res, next) {
+  const myData = {
+    name: "Sem",
+    title: "My Personal site",
+    say: "hello!!!",
+  };
+  res.render("myview", { ...myData });
 });
 
 module.exports = router;
